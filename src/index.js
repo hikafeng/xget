b92a044c84cf542f14f466599de300469377467a
Hika风 <hikafeng@163.com>
2025-09-30 09:40:31 +0800

    改成 https://github.com/hikafeng/Xget

--- src/index.js
+++ src/index.js
@@ -293,7 +293,7 @@ async function handleRequest(request, env, ctx) {
 
     // Redirect root path or invalid platforms to GitHub repository
     if (url.pathname === '/' || url.pathname === '') {
-      const HOME_PAGE_URL = 'https://github.com/xixu-me/Xget';
+      const HOME_PAGE_URL = 'https://github.com/hikafeng/Xget';
       return Response.redirect(HOME_PAGE_URL, 302);
     }
 
@@ -333,7 +333,7 @@ async function handleRequest(request, env, ctx) {
       }) || effectivePath.split('/')[1];
 
     if (!platform || !config.PLATFORMS[platform]) {
-      const HOME_PAGE_URL = 'https://github.com/xixu-me/Xget';
+      const HOME_PAGE_URL = 'https://github.com/hikafeng/Xget';
       return Response.redirect(HOME_PAGE_URL, 302);
     }
 
